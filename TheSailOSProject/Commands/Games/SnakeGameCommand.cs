c3e615cba43b20f6739a5182e48205bc9c957807
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-02-21 16:11:02 +0200

    added SnakeGame to process...

--- TheSailOSProject/Commands/Games/SnakeGameCommand.cs
+++ TheSailOSProject/Commands/Games/SnakeGameCommand.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
 using TheSailOSProject.Games.Snake;
+using TheSailOSProject.Processes;
+using TheSailOSProject.Processes.Applications;
 
 namespace TheSailOSProject.Commands.Games;
 
@@ -7,15 +10,14 @@ public class SnakeGameCommand : ICommand
 {
     public void Execute(string[] args)
     {
-        Console.Clear();
-        Console.WriteLine("Starting Snake Game...");
-        Console.WriteLine("Use arrow keys to move, ESC to exit");
-        Console.WriteLine("Press any key to start...");
-        Console.ReadKey(true);
+        var snakeProcess = new SnakeProcess();
+        ProcessManager.Register(snakeProcess);
+        ProcessManager.Start(snakeProcess);
 
-        var game = new SnakeGame();
-        game.Run();
-
-        Console.Clear();
+        while (snakeProcess.IsRunning)
+        {
+            ProcessManager.Update();
+            Thread.Sleep(100);
+        }
     }
 }
---
+++ TheSailOSProject/Processes/Applications/SnakeProcess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TheSailOSProject.Games.Snake;
+using TheSailOSProject.Styles;
+
+namespace TheSailOSProject.Processes.Applications;
+
+public class SnakeProcess : Process
+{
+    private SnakeGame _game;
+    private bool _exitGame = false;
+
+    public SnakeProcess() : base("SnakeGame", ProcessType.Application)
+    {
+        _game = new SnakeGame();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        Console.Clear();
+        Console.WriteLine("Starting Snake Game...");
+        Console.WriteLine("Use arrow keys to move, ESC to exit");
+        Console.WriteLine("Press any key to start...");
+        Console.ReadKey(true);
+    }
+
+    public override void Run()
+    {
+        _game.Run();
+        Stop();
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+        Console.Clear();
+        ConsoleManager.WriteLineColored("[SnakeGame] Exited", ConsoleStyle.Colors.Primary);
+    }
+}
